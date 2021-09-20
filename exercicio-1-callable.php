add8cd81c8fa7702d576e8f87f081ba771613c9b
joao-antonio-gomes <joaoantoniog96@gmail.com>
2021-09-20 09:46:36 -0300

    Passing a variable into a closure with 'use'

--- exercicio-1-callable.php
+++ exercicio-1-callable.php
@@ -1,26 +1,36 @@
 <?php
 
-function sayHello(): string
+function sayHello()
 {
     return "Hello World\n";
 }
 
-function executeFunction(callable $function): void
+function executeFunction(callable $anotherFunction)
 {
     echo "Calling another function\n";
-    echo $function();
+    echo $anotherFunction();
 }
 
 executeFunction('sayHello');
 
+
+/**
+ * Every anon function is a Closure Object
+ * With bindTo method we can connect an anon function with an object
+ */
 // Anon functions
 executeFunction(function () {
     return "Anon function\n";
 });
 
+$variable = "Test \n";
+// Using a variable from another escope, only works in a closure (anon function)
 // Anon function stored on a variable
-$anonFunction = function () {
+$anonFunction = function () use ($variable) {
+    echo $variable;
     return "Anon function in a variable\n";
 };
 
 executeFunction($anonFunction);
+
+
